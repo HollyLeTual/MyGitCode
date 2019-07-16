9012759cc502e86b7531ea2c0b25d400435d7dad
HollyLeTual <holly.letual@inmarsat.com>
2019-07-16 12:27:52 -0230

    Changes Made to APP.Java Test Case

--- src/main/java/QualiTest/App.java
+++ src/main/java/QualiTest/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Today is a good day!" );
+        System.out.println( "Happy 40th Birthday Inmarsat" );
     }
 }
