75fc63ed83a528c101a8a2fc6841b51d4b258189
HollyLeTual <holly.letual@inmarsat.com>
2019-07-17 07:15:50 -0230

    Architect 1 code changes

---
+++ src/main/java/QualiTest/AppGitTest.java
@@ -0,0 +1,15 @@
+package QualiTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppGitTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Today is a good day!" );
+        System.out.println( "Happy 40th Birthday Inmarsat" );
+    }
+}
---
+++ src/main/java/QualiTest/DesktopTestCase2.java
@@ -0,0 +1,21 @@
+package QualiTest;
+
+import org.junit.Test;
+
+public class DesktopTestCase2 {
+	
+	@Test
+	public void desktoptestcase1() {
+		
+		System.out.println("This is a UI Test Case 1");
+		System.out.println("This is a UI Test Case 1.1");
+		
+	}
+
+	@Test
+	public void desktoptestcase2() {
+		
+		System.out.println("This is a UI Test Case 2");
+		System.out.println("This is a UI Test Case 2.1");
+	}
+}
